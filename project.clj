39ec4ecdb746d205d6b5b381ad90f6b8a1e84109
Ryan Thomas <r.n.thomas@gmail.com>
2013-07-05 19:58:55 +0000

    Add SNAPSHOT to version string

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clucy "0.4.0-rthomas-1"
+(defproject clucy "0.4.0-rthomas-1-SNAPSHOT"
   :description "A Clojure interface to the Lucene search engine"
   :url "http://github/weavejester/clucy"
   :dependencies [[org.clojure/clojure "1.4.0"]
